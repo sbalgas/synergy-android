0a2627f5b49377509253de436a9a008592ec39fa
SuperBalgas <leandro@g-ortega.com.ar>
2015-02-21 18:03:14 -0300

    * Agregando Multilenguaje.
    ------------------------------------------
    
    * Adding Multilanguage.

--- gen/org/synergy/R.java
+++ gen/org/synergy/R.java
@@ -14,21 +14,30 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int clientNameEditText=0x7f050001;
-        public static final int clientNameTextView=0x7f050000;
-        public static final int connectButton=0x7f050008;
-        public static final int inputDeviceEditText=0x7f050007;
-        public static final int inputDeviceTextView=0x7f050006;
-        public static final int outputEditText=0x7f050009;
-        public static final int serverHostEditText=0x7f050003;
-        public static final int serverHostTextView=0x7f050002;
-        public static final int serverPortEditText=0x7f050005;
-        public static final int serverPortTextView=0x7f050004;
+        public static final int clientNameEditText=0x7f050002;
+        public static final int clientNameTextView=0x7f050001;
+        public static final int connectButton=0x7f050009;
+        public static final int inputDeviceEditText=0x7f050008;
+        public static final int inputDeviceTextView=0x7f050007;
+        public static final int outputEditText=0x7f05000b;
+        public static final int outputEditTextView=0x7f05000a;
+        public static final int scrollView1=0x7f050000;
+        public static final int serverHostEditText=0x7f050004;
+        public static final int serverHostTextView=0x7f050003;
+        public static final int serverPortEditText=0x7f050006;
+        public static final int serverPortTextView=0x7f050005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int Str_clientNameTextView=0x7f040001;
+        public static final int Str_connectButton=0x7f040005;
+        public static final int Str_disconnectButton=0x7f040006;
+        public static final int Str_inputDeviceTextView=0x7f040004;
+        public static final int Str_outputEditTextView=0x7f040007;
+        public static final int Str_serverHostTextView=0x7f040002;
+        public static final int Str_serverPortTextView=0x7f040003;
         public static final int app_name=0x7f040000;
     }
 }
